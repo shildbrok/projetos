11a8e7ee912fb10573694b670508dea3a94c82b8
shildbrok <estevan.moreiraa2@gmail.com>
2023-03-15 18:44:16 -0300

    Adicionando Pages

--- estar-fans/src/App.js
+++ estar-fans/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+
+import Home from './pages/Home'
+import About from './pages/About'
+
+import {BrowserRouter, Router, Route} from 'react-router-dom'
+
+
 function App() {
   return (
     <div className="App">
       <title>Estar Fans</title>
       <h1>Olá Robert, Sid, e Irmãos Jujudson's</h1>
       <h2 className="subtitle">aqui é o teste</h2>
+      <BrowserRouter>
+        <Router>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<About/>}/>
+        </Router>
+      </BrowserRouter>
     </div>
+    
   );
 }
 
